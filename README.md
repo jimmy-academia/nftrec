4ccb7e2e9e7fa14e269acf2e91757ad4605aa8ce
cyyeh <d09942009@ntu.edu.tw>
2024-10-23 17:42:43 +0800

    just another commitment to greatness

---
+++ README.md
@@ -0,0 +1,8 @@
+# nft recommendation
+
+## experiment log
+
+> insert new experiment log from the top
+
+### 2024-10-23
+inialize repository
---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+def main():
+
+    print('same structure: main => experiment => module files')
+    print('but clean up the module files')
+    print('start with main experiment')
+    pass
+
+
+if __name__ == '__main__':
+    main()
